698ce4a948ceb7213121906ac86889711ba3ed36
Farukul Islam <farukgb1999@gmail.com>
2023-04-17 10:05:31 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEcMi1_yMNnuB4sYoxcpcRw-9gvYhjDOQ",
+  authDomain: "ema-john-firebase-auth-b03e1.firebaseapp.com",
+  projectId: "ema-john-firebase-auth-b03e1",
+  storageBucket: "ema-john-firebase-auth-b03e1.appspot.com",
+  messagingSenderId: "715215109770",
+  appId: "1:715215109770:web:25b53cb9be625e54a0b773"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
