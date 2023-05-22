edca912892fa37345d9220439ae0a66321a79a9f
Farukul Islam <farukgb1999@gmail.com>
2023-05-22 19:04:42 +0600

    data load from data page

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -4,7 +4,7 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './ReviewItem.css';
 
 const ReviewItem = ({ product, handleRemoveFromCart }) => {
-    const { id, img, price, name, quantity } = product;
+    const { _id, img, price, name, quantity } = product;
     return (
         <div className='rounded flex my-3 items-center p-3 gap-2 md:w-[571px] border-2 border-gray-500'>
             <img className='w-20' src={img} alt="" />
@@ -13,7 +13,7 @@ const ReviewItem = ({ product, handleRemoveFromCart }) => {
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
             </div>
-            <button onClick={() => handleRemoveFromCart(id)} className='btn-delete'>
+            <button onClick={() => handleRemoveFromCart(_id)} className='btn-delete'>
                 <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
             </button>
         </div>
--- src/fb.js
+++ src/fb.js
@@ -15,7 +15,7 @@
 //   (function(d, s, id){
 //      var js, fjs = d.getElementsByTagName(s)[0];
 //      if (d.getElementById(id)) {return;}
-//      js = d.createElement(s); js.id = id;
+//      js = d.createElement(s); js._id = id;
 //      js.src = "https://connect.facebook.net/en_US/sdk.js";
 //      fjs.parentNode.insertBefore(js, fjs);
 //    }(document, 'script', 'facebook-jssdk'));
